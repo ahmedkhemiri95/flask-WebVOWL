e004b21b385182ced1955e66e154ab5ee1367aa0
Ahmed khémiri <ahmedkhemiri24@outlook.fr>
2021-06-13 19:10:36 +0100

    Update owl2vowl.py

--- application/owl2vowl.py
+++ application/owl2vowl.py
@@ -5,12 +5,12 @@ import shutil
 def convert():
 
     # Run owl2vowl.jar converter : convert ontology file TTL to JSON format supported by WebVOWL
-    proc = subprocess.Popen("java -jar ../flask-WebVOWL/application/static/owl2vowl.jar -file ../flask-WebVOWL/application/static/data/YOUR_ONTOLOGY.ttl", shell=True, stdout=subprocess.PIPE) 
+    proc = subprocess.Popen("java -jar ../application/static/owl2vowl.jar -file ../application/static/data/YOUR_ONTOLOGY.ttl", shell=True, stdout=subprocess.PIPE) 
     script_response = proc.stdout.read() 
     script_response=script_response.decode("utf-8")
 
     # Move generated JSON file to data dir 
-    source = '../flask-WebVOWL/YOUR_ONTOLOGY.json' 
-    destination = '../flask-WebVOWL/application/static/data/YOUR_ONTOLOGY.json'
+    source = '../YOUR_ONTOLOGY.json' 
+    destination = '../application/static/data/YOUR_ONTOLOGY.json'
     if os.path.exists(source):
      dest = shutil.move(source, destination) 
