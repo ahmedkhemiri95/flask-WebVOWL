c72b0f6724e98862f0d3b75a71c187f7c737c56c
Ahmed khémiri <ahmedkhemiri24@outlook.fr>
2021-06-15 01:54:13 +0100

    Update owl2vowl.py

--- application/owl2vowl.py
+++ application/owl2vowl.py
@@ -6,9 +6,7 @@ def convert():
 
     # Run owl2vowl.jar converter : convert ontology file TTL to JSON format supported by WebVOWL
     proc = subprocess.Popen("java -jar ../application/static/owl2vowl.jar -file ../application/static/data/YOUR_ONTOLOGY.ttl", shell=True, stdout=subprocess.PIPE) 
-    script_response = proc.stdout.read() 
-    script_response=script_response.decode("utf-8")
-
+    
     # Move generated JSON file to data dir 
     source = '../YOUR_ONTOLOGY.json' 
     destination = '../application/static/data/YOUR_ONTOLOGY.json'
